d8393c7850e03be19ef0af33534b823ab8fb0351
Liangzhong Yin <ryanyintest@gmail.com>
2021-04-15 10:55:15 -0700

    Update server

--- client.py
+++ client.py
@@ -5,9 +5,39 @@ if ":" not in ip:
 if "http" not in ip:
     ip = "http://" + ip
 while True:
-    data = input("What program to kill? Get Processes to get the current processes running. ")
-    if data.lower() == "Get Processes".lower():
-        r = requests.get(ip + "/getprocesses")
+    data = input("Pick one of the following: \n\t[1] Get Processes\n\t[2] Get Windows\n\t[3] Close Process\n\t[4]Close Window")
+    try:
+        data = int(data)
+    except ValueError:
+        print("You did not input a number, please enter 1, 2, 3, or 4")
+        continue
+    if data not in [1, 2, 3, 4]:
+        print("You did not input a number, please enter 1, 2, 3, or 4")
+        continue
+    if data == 1:
+        r = requests.get(f"{ip}/getprocesses")
         print(r.text)
+    elif data == 2:
+        r = requests.get(f"{ip}/getwindows")
+        if r.status_code == 501:
+            print("The server cannot handle the request, likely because the server isn't a Windows computer")
+        print(r.text)
+    elif data == 3:
+        process = input("What process to kill? ")
+        r = requests.post(ip, json = {'run': process})
+        if r.status_code == 400:
+            print("Unsuccessful because there was no process to close, if you're on Windows this is likely because you did not append .exe to the end of the string")
+            continue
+        print(f"Succesfully closed {process}")
     else:
-        requests.post(ip, json={"run": data})
+        window = input("What window to close? ")
+        r = requests.post(f"{ip}/closewindow" json = {'wintoclose': window})
+        if r.status_code == 501:
+            print("The server cannot handle the request, likely because the server isn't a Windows computer")
+            continue
+        if r.status_code == 400:
+            print("Unsuccessful because there was no process to close, if you're on Windows this is likely because you did not append .exe to the end of the string")
+            continue
+        print(f"Succesfully closed all windows with names containing: {process}")
+
+
